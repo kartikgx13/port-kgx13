216ec5668f0f2e4fd9994033768995d3512321da
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2023-04-07 23:03:20 +0530

    kartik gavande 5th commit date: 07/04/2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '../../components/Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceAngry,faInstagram } from '@fortawesome/free-solid-svg-icons'
+import { FacebookIcon, FacebookShareButton, LinkedinIcon, LinkedinShareButton } from 'next-share'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +16,8 @@ export default function Home() {
       <section className="main-hero-section">
         <div className="main-hero-container">
           <div className="hero-section-left">
-            
           </div>
           <div className="hero-section-right">
-
           </div>
         </div>
       </section>
