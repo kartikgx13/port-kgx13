701ccea6c76c00f06d6e6fe56c44830db38acb1f
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2023-05-15 21:15:50 +0530

    kartik gavande 21st commit date: 15/05/2023

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -6,20 +6,31 @@ function Timeline() {
      <div className="timeline">
       <div className="timeline_container left">
         <div className="timeline-content">
-          <h2>Year</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias quos, numquam voluptatem nobis totam libero magnam molestias hic consequuntur excepturi ducimus a nostrum, dolores, vitae ad at non animi!</p>
+          <h2>2008-2018</h2>
+          <p>School Name: Children&#39;s Academy <br/>
+            Board: ICSE <br/>
+            10th Percentage: 95&#46;67&#37;
+          </p>
         </div>
       </div>
       <div className="timeline_container right">
         <div className="timeline-content">
-          <h2>Year</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias quos, numquam voluptatem nobis totam libero magnam molestias hic consequuntur excepturi ducimus a nostrum, dolores, vitae ad at non animi!</p>
+          <h2>2018-2020</h2>
+          <p>
+            Jr&#46; College: Thakur College of Science and Commerce <br/>
+            Board: HSC <br/>
+            12th Percentage: 88&#37; <br/>
+            Main Elective: Computer Science
+          </p>
         </div>
       </div>
       <div className="timeline_container left">
         <div className="timeline-content">
-          <h2>Year</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum alias quos, numquam voluptatem nobis totam libero magnam molestias hic consequuntur excepturi ducimus a nostrum, dolores, vitae ad at non animi!</p>
+          <h2>2020-2024</h2>
+          <p>
+            College: Thakur College of Engineering and Technology <br/>
+            Branch: Computer Science
+          </p>
         </div>
       </div>
      </div>
